fd4082500c782513ce9a90d69d7a4cda525968df
Uallace Macedo <uallace.macedo@outlook.com>
2025-11-04 21:20:11 -0300

    feat: adicao de login/registro

--- src/app/pages/register/page.js
+++ src/app/pages/register/page.js
@@ -1,7 +1,22 @@
+import LoginRegisterSelector from "../../components/LoginRegisterSelector.js";
+
 const RegisterPage = () => {
   return (`
     <div class="container">
-      <h1 class="h1">RegisterPage</h1>
+      <div class="flex-center lr-main h-full">
+        <div class="flex-center lr-header">
+          <div class="flex-center lr-img-container">
+            <img src="/public/assets/pap-logo.png" alt="pap logo" class="pap-logo" />
+          </div>
+          <div class="lr-header-text">
+            <h1 class="h1">Entre agora!</h1>
+            <p class="caption text-center">Crie uma conta ou faça login para acessar a plataforma.</p>
+          </div>
+        </div>
+        <div class="w-full lrs-form">
+          ${LoginRegisterSelector("register")}
+        </div>
+      </div>
     </div>
   `)
 }
