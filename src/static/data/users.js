eab7b800e755e26c5438f120bca4ea23ba5a7002
Uallace Macedo <uallace.macedo@outlook.com>
2025-11-07 17:33:37 -0300

    refactor: ajuste na logica do localstorage

--- src/static/data/users.js
+++ src/static/data/users.js
@@ -1,10 +1,11 @@
 export default [
   {
     id: 0,
-    cpf: "000.000.000-00",
+    cpf: "00000000000",
     name: "admin",
     last_name: "adm",
     email: "admin@email.com",
-    password: "admin"
+    password: "admin",
+    image_url: "https://api.dicebear.com/7.x/adventurer/svg?seed=admin"
   },
 ]
