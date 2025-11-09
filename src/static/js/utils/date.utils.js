1173a43328a7f34c3cbdbf169e310ba42295dfc3
HCmenezes <hectorfrancamenezes@gmail.com>
2025-11-09 13:43:22 -0300

    Validação do de cpf ao registrar

--- src/static/js/utils/date.utils.js
+++ src/static/js/utils/date.utils.js
@@ -8,6 +8,28 @@ export function checkYears(date, minAge) {
   return age >= minAge;
 }
 
+export function testCPF(cpf) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+    let i;
+  if (cpf == "00000000000") return false;
+
+  for (i=1; i<=9; i++) Soma = Soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(cpf.substring(9, 10)) ) return false;
+
+  Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(cpf.substring(10, 11) ) ) return false;
+    return true;
+}
+
 export function todayDate() {
   const [date, _] = new Date().toISOString().split("T");
   const [year, month, day] = date.split("-");
