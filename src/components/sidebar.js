c197d2d4e210658706431b14bbd2f64db50eca77
Uallace Macedo <uallace.macedo@outlook.com>
2025-11-09 07:22:55 -0300

    feat: ajuste de home & responsividade

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -8,34 +8,41 @@ const sidebar = () => {
   const userGroups = groups.filter(g => g.users_id.includes(id));
 
   return (`
-    <nav class="sidebar">
-      <div class="sidebar-header">
-        <img src="${image_url}" alt="user-icon" class="sidebar-header__user-profile">
-        <p>${name} ${last_name}</p>
-      </div>
-      <hr/>
-      <div class="sidebar-main">
-        <a class="sidebar-link" href="#home"><img src="/static/assets/icons/nav-home.svg" class="sidebar-icon" />Home</a>
-        <a class="sidebar-link" href="#notifications"><img src="/static/assets/icons/nav-notification.svg" class="sidebar-icon" />Notificacoes</a>
-        <a class="sidebar-link" href="#profile"><img src="/static/assets/icons/sidebar-profile.svg" class="sidebar-icon" />Perfil</a>
-        <a class="sidebar-link" href="#admin"><img src="/static/assets/icons/sidebar-admin.svg" class="sidebar-icon" />Admin</a>
-        <details class="sidebar-community">
-          <summary>Comunidades</summary>
-          <div class="sidebar-communities">
-            ${userGroups.length == 0 ? ("n tem grupos") : userGroups.map(g => (`
-              <a href="#group?id=${g.id}" class="sidebar-community__group">
-                <img src="${g.image_url}" alt="group-img" class="sidebar-community__group-img" />
-                <p>${g.name}</p>
-              </a>
-            `))}
-          </div>
-        </details>
-      </div>
-      <hr />
-      <div class="sidebar-footer">
-        footer
-      </div>
-    </nav>  
+    <div id="sidebar-overlay" class="sidebar-overlay visible" onclick="closeSideBar()"></div>
+    <div id="sidebar-wrapper" class="sidebar-wrapper open">
+      <nav class="sidebar">
+        <div class="sidebar-header">
+          <img src="${image_url}" alt="user-icon" class="sidebar-header__user-profile">
+          <img src="/static/assets/icons/close.svg" class="sidebar-header__close" onclick="closeSideBar()" />
+          <p>${name} ${last_name}</p>
+        </div>
+        <hr/>
+        <div class="sidebar-main">
+          <a class="sidebar-link" href="#home"><img src="/static/assets/icons/nav-home.svg" class="sidebar-icon" />Home</a>
+          <a class="sidebar-link" href="#notifications"><img src="/static/assets/icons/nav-notification.svg" class="sidebar-icon" />Notificacoes</a>
+          <a class="sidebar-link" href="#profile"><img src="/static/assets/icons/sidebar-profile.svg" class="sidebar-icon" />Perfil</a>
+          ${admin ? `<a class="sidebar-link" href="#admin"><img src="/static/assets/icons/sidebar-admin.svg" class="sidebar-icon" />Admin</a>` : ''}
+          <details class="sidebar-community">
+            <summary>Comunidades</summary>
+            <div class="sidebar-communities">
+              ${userGroups.length == 0 ? (`<div class="sidebar-communities no-community"><a href="#community">Buscar comunidades</a></div>`) : userGroups.map(g => (`
+                <a href="#group?id=${g.id}" class="sidebar-community__group">
+                  <img src="${g.image_url}" alt="group-img" class="sidebar-community__group-img" />
+                  <p>${g.name}</p>
+                </a>
+              `))}
+            </div>
+          </details>
+        </div>
+        <hr />
+        <div class="sidebar-footer">
+          <a class="sidebar-link" href="#notifications" onclick="logout()"1>
+            <img src="/static/assets/icons/logout.svg" class="sidebar-icon" />
+            Sair
+          </a>
+        </div>
+      </nav>
+    </div>
   `)
 }
 
