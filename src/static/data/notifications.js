228489b3fa8b6147944347ba0d6193332f55c11f
Uallace Macedo <uallace.macedo@outlook.com>
2025-11-10 22:59:13 -0300

    mock

--- src/static/data/notifications.js
+++ src/static/data/notifications.js
@@ -1,5 +1,5 @@
 export default [
-  { id: 0, title: "Notificacao TEST", message: "Descricao move para home", target: [0], moveTo: "#home", date: "07/11/2025", saw: [] },
-  { id: 1, title: "Notificacao TEST 2", message: "move para #group0", target: [1, 2, 3], moveTo: "#group?id=0", date: "06/11/2025", saw: [] },
-  { id: 2, title: "Notificacao TEST 3", message: "move pra #group2", target: [1, 2, 3], moveTo: "#group?id=2", date: "05/11/2025", saw: [] },
+  { id: 0, title: "Notificacao TEST 1", message: "Bem-vindo a PAP.com - Move para home", target: [0], moveTo: "#home", date: "07/11/2025", saw: [] },
+  { id: 1, title: "Notificacao TEST 2", message: "Bem-vindo a comunidade SALVADOR", target: [0, 2, 3], moveTo: "#group?id=0", date: "06/11/2025", saw: [] },
+  { id: 2, title: "Notificacao TEST 3", message: "Notificacao vista", target: [0], moveTo: null, date: "05/11/2025", saw: [0] },
 ]
